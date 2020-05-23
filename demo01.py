748f68a89173f25d766152d5a4f3211484ed45fa
EddieSwan <q2111304@gmail.com>
2020-05-23 22:05:37 +0800

    第一次向Github提交代码

---
+++ demo01.py
@@ -0,0 +1,63 @@
+"""
+print("hello world!") # 字符串
+print(2333) # 整数
+print(2.333) # 小数
+print(True) # 布尔值 Trur or False
+print(())  # 元组
+print([])   # 数组
+print({})   # 字典
+锄禾日当午
+汗滴禾下土
+print("哈哈",2333)
+print("哈哈"+"嘻嘻") #字符串的拼接
+print("哈哈"*100)
+print(((1+2)*100-9.9)/2) 
+print(2<3)
+
+# 变量 
+#赋值
+name = "张三" #把张三的这个值赋值给了名字叫a的这个变量
+print(name)
+"""
+"""
+a = input("请输入:")
+b = input("请输入:")
+print("input获取的内容: ",a+b)
+"""
+#数据格式的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = str(2333)
+# print(type(a))
+
+# a = int("哈哈")
+# print(type(a))
+
+# a = float(input("请输入:"))
+# b = float(input("请输入:"))
+# print("input获取的内容: ",a+b)
+
+
+# a = 'dsfsdfdsfsdgdfhfgjhfgdjjerhuwrhywhwghfdgsdhdfh   rfdgdfh'
+
+# print(len(a))
+
+
+
+"""
+练习:通过代码获取两段内容，并且计算它们的长度的和。
+
+"""
+
+a = input("请输入:")
+b = input("请输入:")
+print("input获取的内容: ",a+b)
+print(len(a+b))
+
+
